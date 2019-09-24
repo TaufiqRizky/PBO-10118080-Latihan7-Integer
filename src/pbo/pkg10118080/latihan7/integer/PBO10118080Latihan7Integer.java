ab602cb24be292f9d8641be48dd1be6d3e460c47
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:32:50 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan7/integer/PBO10118080Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan jenis-jenis integer
+ */
+public class PBO10118080Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b=120;
+        s=32767;
+        i=1_000_000_000;
+        l=90000000000L;
+        System.out.println("byte : "+b);
+        System.out.println("short : "+s);
+        System.out.println("int : "+ i);
+        System.out.println("long : "+l);
+    }
+    
+}
